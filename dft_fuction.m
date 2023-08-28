322a86a0073d50c7580762b6b1789f1509e95003
yoomimi <139429073+yoomimi@users.noreply.github.com>
2023-08-28 14:56:02 +0900

    Update dft_fuction.m

--- dft_fuction.m
+++ dft_fuction.m
@@ -1,3 +1,11 @@
+g=rand(10,1)
+#직접 만든 DFT 함수로 Fourier Transform
+A = DFT(g)
+#matlab 내장함수로 Fast Fourier Transform
+B = fft(g)
+
+
+#직접 만든 DFT 함수
 function G = DFT(g)
     M = length(g);
     W = exp(-1j*2*pi/M*(0:M-1)'*(0:M-1));
